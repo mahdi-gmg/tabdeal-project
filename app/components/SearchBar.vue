f4ac6eb8317a23e54c8d6b7fd808a85562d9ba10
mahdi-gmg <mahdi.ghanbari.eng@gmail.com>
2026-02-03 21:36:44 +0330

    modify the toml file

--- app/components/SearchBar.vue
+++ app/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
     <form @submit.prevent="onSubmit" class="search flex flex-row gap-4 rounded-lg p-4 relative">
         <input v-model="text" class="py-3 pr-2 pl-4 rounded-sm flex-1" type="text" placeholder="Search for characters..." />
-        <button class="search-button flex flex-row py-3 px-4 rounded-sm gap-2">
+        <button type="submit" class="search-button flex flex-row py-3 px-4 rounded-sm gap-2">
             <img src="~/assets/icons//search-normal.svg"  width="14" height="14"/>
             <span class="hidden md:inline-block">Search</span>
         </button>
